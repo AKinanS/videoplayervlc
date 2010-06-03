f944206ee5fcc868e99f647a4f7d5b2d9f62b873
sirglorg <sirglorg@19f9429b-519e-ab73-8b1e-29949bd98282>
2010-06-03 14:12:38 +0000

    bin files excluded

--- VideoPlayer/Window1.xaml.cs
+++ VideoPlayer/Window1.xaml.cs
@@ -20,12 +20,28 @@ namespace VideoPlayer {
 	/// Interaction logic for Window1.xaml
 	/// </summary>
 	public partial class Window1 : Window {
+
+		/// <summary>
+		/// actual video volume
+		/// </summary>
 		private int Volume {
 			get;
 			set;
 		}
 
+		/// <summary>
+		/// the VLC library id of a last played video
+		/// </summary>
+		public int LastPlayedID {
+			get;
+			set;
+		}
+
 		private bool _isPlaying;
+		/// <summary>
+		/// tells whether the video is being played at the time and sets
+		/// the play button appearence according to the current state
+		/// </summary>
 		private bool IsPlaying {
 			get {
 				return _isPlaying;
@@ -40,6 +56,18 @@ namespace VideoPlayer {
 			}
 		}
 
+		/// <summary>
+		/// tells whether the video is stopped at the time - ie. if true,
+		/// than no video is currently loaded and therefore can't be manipulated with.
+		/// </summary>
+		public bool IsStopped {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// tells whether the some video is currently loaded in the library
+		/// </summary>
 		bool IsOpened {
 			get;
 			set;
@@ -55,16 +83,25 @@ namespace VideoPlayer {
 			Volume = 50;
 			IsOpened = false;
 			IsPlaying = false;
+			IsStopped = true;
 		}
 
 		private void play_Click(object sender, RoutedEventArgs e) {
-			if (IsOpened) {
-				if (IsPlaying) {
-					togglePause();	//triggers pause
-				
+			if (IsOpened) { // there is some video in the playlist
+				if (IsStopped) {	// the video is not running - play from beginning
+					playVideo(LastPlayedID);
 				} else {
-                    togglePause();  //triggers play
+					if (IsPlaying) {	//the video is running - toggle between pause & play
+						axVLC.playlist.togglePause();	//triggers pause
+						IsPlaying = false;
+					} else {
+						axVLC.Volume = Volume;
+						axVLC.playlist.togglePause();	//triggers play
+						IsPlaying = true;
+					}
 				}
+			} else {
+				openVideo();
 			}
 
 		}
@@ -76,9 +113,19 @@ namespace VideoPlayer {
 
 		private void stop_Click(object sender, RoutedEventArgs e) {
 			stopCurrentVideo();
+			
 		}
 
 		private void open_Click(object sender, RoutedEventArgs e) {
+			openVideo();
+		}
+
+		/// <summary>
+		/// opens the video via openFileDialog and adds it into the library
+		/// </summary>
+		/// <returns>playlist id of the added video file</returns>
+		private void openVideo() {
+			int id = -1;
 			OpenFileDialog openFileDialog = new OpenFileDialog();
 			openFileDialog.Filter = "Video Files|*.avi;*.mpg;*.mpeg";
 			openFileDialog.Title = "Select a Video File";
@@ -87,19 +134,36 @@ namespace VideoPlayer {
                 progressSlider.Value = 0;
 				stopCurrentVideo();
 				axVLC.playlist.clear();
-				//Thread.Sleep(5000);
-				int id = axVLC.playlist.add(openFileDialog.FileName, openFileDialog.Title, new String[] { });
-				axVLC.playlist.playItem(id);
-				IsPlaying = true;
+				id = axVLC.playlist.add(openFileDialog.FileName, openFileDialog.Title, new String[] { });
 				IsOpened = true;
+				if (IsPlaying) {
+					stopCurrentVideo();
+				}
+				playVideo(id);
 			}
 		}
 
+		/// <summary>
+		/// plays video from playlist according to the given id
+		/// </summary>
+		/// <param name="id">the identificator of the video in th VLC playlist</param>
+		private void playVideo(int id) {
+			axVLC.playlist.playItem(id);
+			LastPlayedID = id;
+			IsPlaying = true;
+			IsStopped = false;
+			}
+
+		/// <summary>
+		/// stops current video in case there is some being played
+		/// </summary>
 		private void stopCurrentVideo() {
 			if (IsOpened) {
-				axVLC.playlist.stop();
-				IsPlaying = false;
-				IsOpened = false;
+				if (!IsStopped) {
+					axVLC.playlist.stop();
+					IsPlaying = false;
+					IsStopped = true;
+				}
 			}
 		}
 
